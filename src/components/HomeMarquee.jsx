476d852452dff0a40409b3ae55d41409f9208c52
goni715 <halesgoni715149@gmail.com>
2024-03-22 00:34:35 +0600

    marquee-edited

--- src/components/HomeMarquee.jsx
+++ src/components/HomeMarquee.jsx
@@ -13,31 +13,31 @@ const HomeMarquee = () => {
         <>
 
 
-                <section className="py-6">
+                <section className="py-8">
                     <div className="grid grid-cols-1">
                         <div className="marquee-inner-wrapper">
                             <Marquee speed={90}>
-                                <div className="mx-0 md:mx-2 w-2/12 md:w-3/12">
-                                    <img className="h-[120px] w-[300px]" src={bkash} alt="marquee"/>
-                                </div>
-                                <div className="mx-0 md:mx-2 w-2/12 md:w-3/12">
-                                    <img className="h-[80px] w-[250px]" src={rocket} alt="marquee"/>
-                                </div>
-                                <div className="mx-0 md:mx-2 w-2/12 md:w-3/12">
-                                    <img className="h-[120px] w-[300px]" src={dbbl} alt="marquee"/>
-                                </div>
-                                <div className="mx-0 md:mx-2 w-2/12 md:w-3/12">
-                                    <img className="h-[130px] w-[250px]" src={paypal} alt="marquee"/>
-                                </div>
-                                <div className="mx-0 md:mx-2 w-2/12 md:w-3/12">
-                                    <img className="h-[130px] w-[320px]" src={cityBank} alt="marquee"/>
-                                </div>
-                                <div className="mx-0  w-2/12 md:w-3/12">
-                                    <img className="h-[150px] w-[330px]" src={nagad} alt="marquee"/>
-                                </div>
-                                <div className="mx-0 md:mx-2 w-2/12 md:w-3/12">
-                                    <img className="h-[120px] w-[250px]" src={brac} alt="marquee"/>
-                                </div>
+                                    <div className="mx-28">
+                                        <img className="h-[110px] w-[150px]" src={bkash} alt="marquee"/>
+                                    </div>
+                                    <div className="mx-24">
+                                        <img className="h-[80px] w-[120px]" src={rocket} alt="marquee"/>
+                                    </div>
+                                    <div className="mx-24">
+                                        <img className="h-[130px] w-[180px]" src={dbbl} alt="marquee"/>
+                                    </div>
+                                    <div className="mx-24">
+                                        <img className="h-[110px] w-[150px]" src={paypal} alt="marquee"/>
+                                    </div>
+                                    <div className="mx-24">
+                                        <img className="h-[110px] w-[150px]" src={cityBank} alt="marquee"/>
+                                    </div>
+                                    <div className="mx-24">
+                                        <img className="h-[120px] w-[150px]" src={nagad} alt="marquee"/>
+                                    </div>
+                                    <div className="mx-24">
+                                        <img className="h-[110px] w-[150px]" src={brac} alt="marquee"/>
+                                    </div>
                             </Marquee>
                         </div>
                     </div>
